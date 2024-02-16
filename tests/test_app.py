cc4d481e802b880be25505df3b5d56e2cd731cf9
cherkasskypavel <fenderik09@mail.ru>
2024-02-16 23:33:16 +0300

    Пишем есты с помощью pytest. Нашел, как без ошибок обращаться к рутам, которые завязаны на запросы к БД. Для этого необходимо в файле с тестами переназначать зависимость получения соединения/сессии. Что сделать: создать отдельную тестовую БД, написать фикстуры создания тестового поста/пользователя для проверки тестами, и подключения к БД, в yield секции удалять таблицы и закрвать соединение. Реализовать в классе следующие тесты: на ошибки, получение токена, на успешность. UPD1. Добавил тестовый конфиг и с тестовой базой. UPD2. Протестил базу, добавил фикстуру создать-удалить таблицу и первый тест на пустой список юзеров.

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,18 +1,48 @@
 
 from fastapi import status
 from fastapi.testclient import TestClient
+import pytest as pt
 from sqlalchemy import create_engine
+from sqlalchemy import MetaData
+from sqlalchemy import Boolean, Column, DateTime, ForeignKey, Integer, String, Table
 
 from app.main import app
 from app.db.database import get_connection
-from tests.testing_config import load_config
+from tests.testing_config import testing_config
 
-test_config = load_config('tests/tests.env')
 
-test_engine = create_engine(test_config.test_db_url)
+_test_engine = create_engine(testing_config['TEST_DB_URL'], future=True, echo=True)
+_testing_metadata = MetaData(bind=_test_engine)
+
+# Тестовые Tables отличаются ограничениями на дефолтные
+# значения, так как в рабочих Tables они не назначены
+# ввиду конфликтов этих значений с уже существующими
+# отдампленными таблицами
+users_table = Table(
+    'users',
+    _testing_metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('email', String, nullable=False, unique=True),
+    Column('hashed_password', String, nullable=False),
+    Column('role', String, default='user'),
+    Column('karma', Integer, server_default='100')
+)
+
+
+posts_table = Table(
+    'posts',
+    _testing_metadata,
+    Column('id', Integer, primary_key=True, autoincrement=True),
+    Column('subject', String, nullable=False),
+    Column('text', String, nullable=False),
+    Column('author_id', ForeignKey('users.id')),
+    Column('post_time', DateTime, nullable=False),
+    Column('is_edited', Boolean, server_default='f'),
+    Column('edited_by', String, nullable=True)
+)
 
 def get_test_connection():
-    connection = test_engine.connect()
+    connection = _test_engine.connect()
     try:
         yield connection
     finally:
@@ -22,9 +52,20 @@ app.dependency_overrides[get_connection] = get_test_connection
 
 client = TestClient(app)
 
-#  Написать фикстуры по созданию/удалению tables 
-def test_testpost():
-    response = client.get('/test_post?post_id=1')
-    assert response.status_code == status.HTTP_200_OK
-    assert response.json()['subject'] == 'первый асинхронный пост'
-    assert response.json()['id'] == 1
+
+@pt.fixture(scope='class', autouse=True)
+def create_test_db():
+    _testing_metadata.create_all()
+    yield
+    _testing_metadata.drop_all()
+
+
+
+#  Написать фикстуры по созданию/удалению tables
+
+class TestApp:
+    def test_user_list(self):
+        response = client.get('/users')
+        assert response.status_code == status.HTTP_200_OK
+        assert response.json() == []
+
--- tests/testing_config.py
+++ tests/testing_config.py
@@ -1,17 +1,29 @@
-from dataclasses import dataclass
-from typing import  Optional
-
-from environs import Env
-
-@dataclass
-class TestConfig:
-    test_db_url: str
 
 
-def load_config(path: Optional[str] = None) -> TestConfig:
-    env = Env()
-    env.read_env(path)
-    return TestConfig(
-        test_db_url=env("TEST_DB_URL"),
+from dotenv import dotenv_values
 
-    )
+
+
+_test_env_path = 'tests/testing.env'
+testing_config = dotenv_values(_test_env_path)
+
+print(testing_config['TEST_DB_URL'])
+# Тестовые Tables отличаются ограничениями на дефолтные
+# значения, так как в рабочих Tables они не назначены
+# ввиду конфликтов этих значений с уже существующими
+# отдампленными таблицами
+
+
+
+# @dataclass
+# class TestConfig:
+#     test_db_url: str
+#
+#
+# def load_config(path: Optional[str] = None) -> TestConfig:
+#     env = Env()
+#     env.read_env(path)
+#     return TestConfig(
+#         test_db_url=env("TEST_DB_URL"),
+#
+#     )
